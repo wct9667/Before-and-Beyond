2e8bdc7e650a972294f3716ac0306486440e90fb
Reaper777 <jayeshsingh62@gmail.com>
2025-04-11 00:02:52 -0400

    added movement and jumping

--- BeforeAndBeyond/Assets/Scripts/Player/PlayerController.cs
+++ BeforeAndBeyond/Assets/Scripts/Player/PlayerController.cs
@@ -8,19 +8,59 @@ namespace Player
         [Header("Input")]
         [SerializeField] private InputReader inputReader;
 
+        [Header("Look")]
         public Camera mainCamera;
         public float sensX;
         public float sensY;
-
         public Transform orientation;
-
         float rotationX;
         float rotationY;
+
+        [Header("Ground Check")]
+        public float playerHeight;
+        public LayerMask whatIsGround;
+        public bool grounded;
+
+        [Header("Movement")]
+        public float moveSpeed;
+        float horizontalInput;
+        float verticalInput;
+        Vector3 moveDirection;
+        Rigidbody rb;
+        public float groundDrag;
+
+        [Header("Jump")]
+        public float jumpForce;
+        public float jumpCooldown;
+        public float airMultiplier;
+        public bool readyToJump;
 
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+            rb = GetComponent<Rigidbody>();
+            rb.freezeRotation = true;
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 moveVector = new Vector2(horizontalInput, verticalInput);   
+            Move(moveVector);
+
+            //ground check
+            grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+            if (grounded)
+            {
+                rb.drag = groundDrag;
+                readyToJump = true;
+            }
+            else
+            {
+                rb.drag = 0;
+                readyToJump = false;
+            }
         }
 
         private void OnEnable()
@@ -42,6 +82,21 @@ namespace Player
     
         private void Move(Vector2 moveVector)
         {
+            horizontalInput = moveVector.x;
+            verticalInput = moveVector.y;
+
+            moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+            rb.AddForce(moveDirection.normalized * moveSpeed * 2f, ForceMode.Force);
+
+            if (grounded)
+            {
+                rb.AddForce(moveDirection.normalized * moveSpeed * groundDrag, ForceMode.Force);
+            }
+            else
+            {
+                rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
+            }
+
             Debug.Log($"Move x:{moveVector.x}, y:{moveVector.y} ");
         }
         
@@ -62,7 +117,18 @@ namespace Player
     
         private void Jump()
         {
+            if (grounded && readyToJump)
+            {
+                readyToJump = false;
+                rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+                Invoke(nameof(ResetJump), jumpCooldown);
+            }
             Debug.Log("Jump");
+        }
+
+        private void ResetJump()
+        {
+            readyToJump = true;
         }
     }
 }
