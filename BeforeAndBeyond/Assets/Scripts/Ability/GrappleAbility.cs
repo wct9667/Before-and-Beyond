e04bd7abc2e2f8f621b84ef03486b45b3452bab6
jack-ryan-keegan <jrk8986@g.rit.edu>
2025-04-12 16:55:49 -0400

    Grapple Applies Up Force

--- BeforeAndBeyond/Assets/Scripts/Ability/GrappleAbility.cs
+++ BeforeAndBeyond/Assets/Scripts/Ability/GrappleAbility.cs
@@ -5,20 +5,21 @@ namespace Ability
     [CreateAssetMenu(menuName = "Player/Abilities/GrappleHookAbility", fileName = "GrappleHookAbilitySO")]
     public class GrappleHookAbility : AbstractAbility 
     {
-        private Vector3 grapplePoint;
+        private RaycastHit hit;
         private int maxDist = 200;
         private LayerMask layerMask;
 
+        private GameObject playerGameObject;
         private Transform camera, player;
+        private Rigidbody playerRB;
 
-        private float speed = 100f;
         private float startTime;
-        private float overTime;
+
+        private MonoBehaviour monoBehavior;
 
         public override void ActivateAbility()
         {
             startTime = Time.time;
-            overTime = startTime + 2f;
 
             Debug.Log("Grapple");
             Debug.Log($"Activating Ability {name}");
@@ -28,26 +29,25 @@ namespace Ability
 
         private void GrappleSetup()
         {
+            monoBehavior = GameObject.FindObjectOfType<MonoBehaviour>();
             camera = Camera.main.transform;
-            player = GameObject.FindGameObjectWithTag("Player").transform;
+            playerGameObject = GameObject.FindGameObjectWithTag("Player");
+            playerRB = playerGameObject.GetComponent<Rigidbody>();
+            player = playerGameObject.transform;
             layerMask = LayerMask.GetMask("whatIsGround");
-
         }
 
         private void GrappleStart()
         {
             GrappleSetup();
 
-            RaycastHit hit;
-
             if (Physics.Raycast(camera.transform.position, camera.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
             {
                 Debug.DrawRay(camera.position, camera.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
                 Debug.Log("-----------------------Did Hit: " + hit.point);
 
-                DrawGrapple(hit);
-
-                GrappleMovement(hit);
+                DrawGrapple();
+                monoBehavior.StartCoroutine(GrappleMovement());
             }
             else
             {
@@ -57,26 +57,22 @@ namespace Ability
 
         }
          
-        private IEnumerator GrappleMovement(RaycastHit hit)
+        private IEnumerator GrappleMovement()
         {
-            //player.transform.position = Vector3.Lerp(camera.position, hit.point, fractionOfJourney);
 
-            while (startTime < overTime)
+            while (Vector3.Distance(player.transform.position, hit.point) > 0.5f)
             {
-                player.transform.position = Vector3.Lerp(player.transform.position, hit.point, (Time.time - startTime) * speed); 
+                player.transform.position = Vector3.Lerp(player.transform.position, hit.point, (Time.time - startTime));
+
+                playerRB.AddForce(0, 0.2f, 0, ForceMode.Impulse);
 
                 yield return null;
+
             }
-
-            yield return new WaitForSeconds(1f);
-
-
-            player.transform.position = hit.point;
+            yield return new WaitForSeconds(2f);
         }
 
-
-
-        private void DrawGrapple(RaycastHit hit)
+        private void DrawGrapple()
         {
             GameObject grappleLine = new GameObject();
             LineRenderer drawLine;
@@ -93,7 +89,7 @@ namespace Ability
             drawLine.SetPosition(0, player.transform.position);
             drawLine.SetPosition(1, hit.point);
 
-            GameObject.Destroy(grappleLine, 1.5f);
+            GameObject.Destroy(grappleLine, .25f);
         }
 
     }
