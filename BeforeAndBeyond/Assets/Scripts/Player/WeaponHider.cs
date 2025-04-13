e35e5b17fd593c09e980c95ddd3607cd2fa98af1
jack-ryan-keegan <jrk8986@g.rit.edu>
2025-04-13 15:12:12 -0400

    Weapons become hidden on switching states
    - Anything inside of FantasyWeaponShell becomes inactive when transitioning to the hacker.
    - Anything inside of SciFiWeaponShell becomes inactive when transitioning to the knight.
    - They become active when their respective character is the current one.

---
+++ BeforeAndBeyond/Assets/Scripts/Player/WeaponHider.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHider : MonoBehaviour
+{
+    private EventBinding<CharacterSwap> characterSwapEventBinding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(gameObject.name == "SciFiWeaponShell")
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        characterSwapEventBinding = new EventBinding<CharacterSwap>(() =>
+        {
+            SetWeaponsActive();
+            EventBus<AbilitiesSwapped>.Raise(new AbilitiesSwapped());
+        });
+        EventBus<CharacterSwap>.Register(characterSwapEventBinding);
+    }
+
+    private void OnDisable()
+    {
+        EventBus<CharacterSwap>.Deregister(characterSwapEventBinding);
+    }
+
+    private void SetWeaponsActive()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(!child.gameObject.activeSelf);
+        }
+    }
+}
