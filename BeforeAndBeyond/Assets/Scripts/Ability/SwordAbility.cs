0e870f730b3122ef4b698399d154f94601c6b372
jack-ryan-keegan <jrk8986@g.rit.edu>
2025-04-13 14:30:41 -0400

    Sword Ability Working
    - In lieu of actual enemies, it just destroys any gameobject with the "whatIsEnemy" layer.

--- BeforeAndBeyond/Assets/Scripts/Ability/SwordAbility.cs
+++ BeforeAndBeyond/Assets/Scripts/Ability/SwordAbility.cs
@@ -5,10 +5,54 @@ namespace Ability
     [CreateAssetMenu(menuName = "Player/Abilities/SwordAbility", fileName = "SwordAbilitySO")]
     public class SwordAbility : AbstractAbility
     {
+        private GameObject sword;
+        private Transform camera;
+        private Animator anim;
+        private float attackReach;
+        private RaycastHit hit;
+        private LayerMask layerMask;
+
         public override void ActivateAbility()
         {
             Debug.Log("Sword");
             Debug.Log($"Activating Ability {name}");
+
+            SwordSlash();
+        }
+
+        public override void Setup()
+        {
+            camera = Camera.main.transform;
+            sword = GameObject.FindGameObjectWithTag("Sword");
+            anim = sword.GetComponent<Animator>();
+            layerMask = LayerMask.GetMask("whatIsEnemy");
+            attackReach = 3.0f;
+        }
+
+        private void SwordSlash()
+        {
+            anim.SetTrigger("Attack");
+            AttackRay();
+        }
+
+        private void AttackRay()
+        {
+            if(Physics.Raycast(camera.transform.position, camera.TransformDirection(Vector3.forward), out hit, attackReach, layerMask))
+            {
+                HitTarget();
+            }
+            else
+            {
+                Debug.Log("No Enemy!");
+            }
+        }
+
+        private void HitTarget()
+        {
+            Debug.Log("Hit an Enemy!");
+
+            //replace with actually dealing damage
+            Destroy(hit.transform.gameObject, .25f);
         }
     }
 }
