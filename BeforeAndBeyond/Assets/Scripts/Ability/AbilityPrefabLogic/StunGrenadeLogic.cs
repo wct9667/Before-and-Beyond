fb6039f0d3c583a38671818929318619b29f168f
jack-ryan-keegan <jrk8986@g.rit.edu>
2025-04-21 10:23:44 -0400

    Changed Stun Visual + Started Joust

--- BeforeAndBeyond/Assets/Scripts/Ability/AbilityPrefabLogic/StunGrenadeLogic.cs
+++ BeforeAndBeyond/Assets/Scripts/Ability/AbilityPrefabLogic/StunGrenadeLogic.cs
@@ -46,10 +46,10 @@ public class StunGrenadeLogic : MonoBehaviour
 
         for(float i = 0f; i < grenadeRadius * 2; i = i + 0.5f)
         {
-            yield return new WaitForSeconds(.005f);
+            yield return new WaitForSeconds(.0025f);
             obj.transform.localScale = new Vector3(i, i, i);
         }
-        yield return new WaitForSeconds(.1f);
+        yield return new WaitForSeconds(.075f);
         Destroy(obj);
         Destroy(gameObject);
         yield return new WaitForSeconds(.1f);
--- BeforeAndBeyond/Assets/Scripts/Ability/JoustAbility.cs
+++ BeforeAndBeyond/Assets/Scripts/Ability/JoustAbility.cs
@@ -7,9 +7,55 @@ namespace Ability
     [CreateAssetMenu(menuName = "Player/Abilities/JoustAbility", fileName = "JoustAbilitySO")]
     public class JoustAbility : AbstractAbility
     {
+        [Header("Ability Attributes")]
+        [SerializeField] private LayerMask enemyLayerMask;
+
+        [Header("Range Per Unit Fell")]
+        [SerializeField] private float movementSpeedScaling = .5f;
+
+        private GameObject joustObject;
+        private GameObject playerGameObject;
+        private Rigidbody playerRB;
+
+        private GameObject sword;
+        private Animator animSword;
+
+        private Transform camera, player;
+        private Animator anim;
+        private RaycastHit hit;
+        private RaycastHit enemyHit;
+
+        private float startTime;
+
+        private MonoBehaviour monoBehavior;
+
         public override void ActivateAbility()
         {
             Debug.Log("-----------------------Joust Activated");
+            StartingAnimation();
+
+        }
+
+        public override void Setup()
+        {
+            camera = Camera.main.transform;
+
+            playerGameObject = GameObject.FindGameObjectWithTag("Player");
+            playerRB = playerGameObject.GetComponent<Rigidbody>();
+            player = playerGameObject.transform;
+
+            joustObject = GameObject.FindGameObjectWithTag("Joust");
+            anim = joustObject.GetComponent<Animator>();
+
+            sword = GameObject.FindGameObjectWithTag("Sword");
+            animSword = sword.GetComponent<Animator>();
+
+        }
+
+        private void StartingAnimation()
+        {
+            animSword.SetTrigger("PutAway");
+            anim.SetTrigger("StartJoust");
         }
     }
 }
